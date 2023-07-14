28a476051819e987aa0571f26fdae59338528de1
Sagar Shah <reachsagarshah@gmail.com>
2023-07-14 19:53:52 +0530

    fix - responsive improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,14 +79,14 @@ export default function Home() {
         <div className="flex w-full flex-col justify-between gap-12 md:flex-row">
           {/* Image */}
           <div className="flex justify-center md:order-first md:justify-end">
-            <div className="relative h-[380px] w-[320px] md:h-[520px] md:w-[440px]">
+            <div className="relative h-[380px] w-[320px] md:h-[460px] md:w-[380px] lg:h-[520px] lg:w-[440px]">
               <Image
                 src={SagarFullPose}
                 alt="Fullpose of Sagar"
-                className="absolute z-10 h-[360px] w-[280px] border-8 border-gray-50 max-md:left-5 md:right-0 md:top-0 md:h-[480px] md:w-[400px]"
+                className="absolute z-10 h-[360px] w-[280px] border-8 border-gray-50 max-md:left-5 md:right-0 md:top-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"
                 style={{ objectFit: 'cover' }}
               ></Image>
-              <div className="absolute h-[360px] w-[320px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[480px] md:w-[400px]"></div>
+              <div className="absolute h-[360px] w-[320px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"></div>
             </div>
           </div>
 
--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -9,7 +9,7 @@ const Container = React.forwardRef<HTMLElement, ContainerProps>(
     return (
       <section
         className={mergeClasses(
-          'w-full bg-gray-0 py-10 md:py-20 2xl:py-24',
+          'w-full bg-gray-0 py-16 md:py-20 2xl:py-24',
           className
         )}
         ref={ref}
